40ff63564339d14ee722e784d077350798657547
Maria Manoela <manoelamvc@hotmail.com>
2021-12-29 11:26:49 -0300

    Change parameter of setNome from nome to nomeEmpresa

--- src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
@@ -1,5 +1,20 @@
 package br.com.alura.gerenciador.servlet;
 
 public class Empresa {
-
+	private Integer id;
+	private String nome;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nomeEmpresa) {
+		this.nome = nomeEmpresa;
+	}
+	
 }
