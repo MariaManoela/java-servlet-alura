5cd1f833f846ead1e8186b9aa6125a3534db9655
Maria Manoela <manoelamvc@hotmail.com>
2021-12-29 11:18:57 -0300

    Create List and adiciona method

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,5 +1,12 @@
 package br.com.alura.gerenciador.servlet;
 
-public class Banco {
+import java.util.List;
 
+public class Banco {
+	private static List<Empresa> lista = new ArrayList<>();
+
+	public void adiciona(Empresa empresa) {
+		lista.add(empresa);
+	}
+	
 }
