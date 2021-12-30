74a9ab25ce81c8f1da228068174d4b9e476baf14
Maria Manoela <manoelamvc@hotmail.com>
2021-12-30 17:11:38 -0300

    Add empresa to jsp request

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -26,6 +26,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 		
 		//chamar o JSP
 		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
 		rd.forward(request, response);
 	}
 
