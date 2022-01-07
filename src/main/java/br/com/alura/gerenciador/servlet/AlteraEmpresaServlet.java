3c4e15c82b52d5f6a1d08f9a92fced250dc1c09c
Maria Manoela <manoelamvc@hotmail.com>
2022-01-07 08:20:04 -0300

    Create servlet structure

--- src/main/java/br/com/alura/gerenciador/servlet/AlteraEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/AlteraEmpresaServlet.java
@@ -1,6 +1,10 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +17,22 @@ public class AlteraEmpresaServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		System.out.println("Alterando empresa");
+		
+		String nomeEmpresa = request.getParameter("nome");
+		String paramDataEmpresa = request.getParameter("data");
+		String paramId = request.getParameter("id");
+		Integer id = Integer.valueOf(paramId);
+		
+		Date dataAbertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(paramDataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
+		System.out.println(id);
 	}
 
 }
