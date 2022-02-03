d39f592e5bee66770a643d1dbe1f85a5d4b86ebd
Maria Manoela <manoelamvc@hotmail.com>
2022-02-03 12:03:03 -0300

    Add http params

--- src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -1,15 +1,19 @@
 package br.com.alura.gerenciador.acao;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
 public class ListaEmpresas {
 	
-	public void executa() {
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 		
