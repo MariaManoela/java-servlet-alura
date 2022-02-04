26000b628e8111e0bcbfe7d9a2a23fc32449a1f2
Maria Manoela <manoelamvc@hotmail.com>
2022-02-04 10:05:22 -0300

    Add AlteraEmpresa and NovaEmpresa objects and executa methods

--- src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.gerenciador.acao.AlteraEmpresa;
 import br.com.alura.gerenciador.acao.ListaEmpresas;
 import br.com.alura.gerenciador.acao.MostraEmpresa;
 import br.com.alura.gerenciador.acao.RemoveEmpresa;
@@ -30,6 +31,16 @@ public class UnicaEntradaServlet extends HttpServlet {
 			
 		} else if(paramAcao.equals("MostraEmpresa")) {
 			MostraEmpresa acao = new MostraEmpresa();
+			acao.executa(request, response);
+		}
+		
+		else if(paramAcao.equals("AlteraEmpresa")) {
+			AlteraEmpresa acao = new AlteraEmpresa();
+			acao.executa(request, response);
+		}
+		
+		else if(paramAcao.equals("NovaEmpresa")) {
+			NovaEmpresa acao = new NovaEmpresa();
 			acao.executa(request, response);
 		}
 	}
