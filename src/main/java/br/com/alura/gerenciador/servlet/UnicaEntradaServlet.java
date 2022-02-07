e62b28470f63cb939cf2896df5b4136da47dd8c4
Maria Manoela <manoelamvc@hotmail.com>
2022-02-07 09:21:40 -0300

    Add logic to UnicaEntradaServlet to implement it to all pages at once

--- src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 import br.com.alura.gerenciador.acao.AlteraEmpresa;
@@ -22,6 +23,12 @@ public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession sessao = request.getSession();
+		boolean usuarioNaoEstaLogado = sessao.getAttribute("usuarioLogado") == null;
+		if(usuarioNaoEstaLogado) {
+			response.sendRedirect("entrada?acao=LoginForm");
+		}
 		
 		String paramAcao = request.getParameter("acao");
 		
